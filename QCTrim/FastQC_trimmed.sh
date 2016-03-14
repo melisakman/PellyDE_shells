70242b9bd0f10150f2d792d2b049e6ffdb50ee16
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2016-03-14 09:01:43 -0700

    partition2

--- QCTrim/FastQC_trimmed.sh
+++ QCTrim/FastQC_trimmed.sh
@@ -5,7 +5,7 @@
 #SBATCH -e /home/makman/Pellies/outs/FastQC_trimmed_stderr.txt
 #SBATCH --mail-type=All
 #SBATCH --mail-user=makman@ucdavis.edu
-#SBATCH -p hi
+#SBATCH -p serial
 #SBATCH --array=0-3
 module load fastqc
 fastqc /home/makman/Rawdata/Pellies_NYU/combined_forward_trimmed_paired.fastq
