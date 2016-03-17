ee64808b91aa205e58ad2c203444888ed6fab8d5
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2016-03-16 17:54:19 -0700

    demultiplex

---
+++ QCTrim/Demultiplex_sabre.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH -D /home/makman/Pellies/Demultiplexed/
+#SBATCH -J sabre
+#SBATCH -o /home/makman/ProExp/outs/demultiplexSabre_Pellies_stdout.txt
+#SBATCH -e /home/makman/ProExp/outs/demultiplexSabre_Pellies_stderr.txt
+#SBATCH --mail-type=All
+#SBATCH --mail-user=makman@ucdavis.edu
+#SBATCH -p serial
+#SBATCH --ntasks=32
+module load sabre
+sabre pe -m 1 -c -f /home/makman/Rawdata/Pellies_NYU/combined_forward_trimmed_paired.fastq -r /home/makman/Rawdata/Pellies_NYU/combined_reverse_trimmed_paired.fastq -b barcodes.sabre.txt -u unknown_barcode1.fastq -w unknown_barcode1.fastq
