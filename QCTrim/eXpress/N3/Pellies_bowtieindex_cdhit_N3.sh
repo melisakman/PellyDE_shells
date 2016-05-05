a4175ff568fe957e856d92e0ca84437c18e465c7
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2016-05-05 13:08:36 -0700

    bowtieindex

--- QCTrim/eXpress/N3/Pellies_bowtieindex_cdhit_N3.sh
+++ QCTrim/eXpress/N3/Pellies_bowtieindex_cdhit_N3.sh
@@ -5,7 +5,7 @@
 #SBATCH -e /home/makman/Pellies/outs/bowtiememindex_cdhit_N3_stderr.txt
 #SBATCH --mail-type=All
 #SBATCH --mail-user=makman@ucdavis.edu
-#SBATCH -p bigmemh
-#SBATCH --ntasks=16
+#SBATCH -p serial
+#SBATCH --ntasks=8
 module load bowtie
 bowtie-build --offrate 1 /home/makman/Pellies/eXpress/bowtie_index/phr1_elichr.fasta bowtie_index_Pellies_N3
